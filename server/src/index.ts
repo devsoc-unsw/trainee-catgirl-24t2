cb0810c201f3e38f415b99d5d80fc5a0d7856908
りん <31785428+rin-senpai@users.noreply.github.com>
2024-08-19 16:43:16 +1000

    Use Drizzle ORM with Bun SQLite
    See docs [here](https://orm.drizzle.team/docs/overview)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,15 @@
-import express, { type Request, type Response } from 'express';
+import express, { type Request, type Response } from "express";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
+
+const sqlite = new Database("sqlite.db");
+const db = drizzle(sqlite);
 
 const app = express();
 const port = 3000;
 
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Hello, Bun!');
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Hello, Bun!");
 });
 
 app.listen(port, () => {
