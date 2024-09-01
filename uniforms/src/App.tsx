bebc66ac0e9df6f1193e9e897646d5e221f341d3
Eve <eve.miles09@gmail.com>
2024-09-01 23:44:16 +1000

    added more setting pages and routed them

--- uniforms/src/App.tsx
+++ uniforms/src/App.tsx
@@ -7,6 +7,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Forms from "./pages/Forms"
 import Settings from "./pages/Settings"
 import ViewForms from "./pages/ViewForms"
+import UserDetails from "./pages/settings/UserDetails"
+import AccountSettings from "./pages/settings/account/AccountSettings"
+import Email from "./pages/settings/account/Email"
+import Username from "./pages/settings/account/Username"
+import Password from "./pages/settings/account/Password"
+import Delete from "./pages/settings/account/Delete"
 
 function App() { 
   return (
@@ -14,6 +20,12 @@ function App() {
 	<Routes>
 	  <Route path="forms" element={<Forms />} />
     <Route path="settings" element={<Settings />} />
+    <Route path="settings/user" element={<UserDetails />} />
+    <Route path="settings/user/account" element={<AccountSettings />} />
+    <Route path="settings/user/account/email" element={<Email />} />
+    <Route path="settings/user/account/username" element={<Username />} />
+    <Route path="settings/user/account/password" element={<Password />} />
+    <Route path="settings/user/account/delete" element={<Delete />} />
     <Route path="viewforms" element={<ViewForms />} />
 	</Routes>
      </BrowserRouter>
--- uniforms/src/pages/Settings.tsx
+++ uniforms/src/pages/Settings.tsx
@@ -1,39 +1,45 @@
-import { Button } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
 import { ChevronRight } from "lucide-react"
-import { Separator } from "@/components/ui/separator"
+import { Link } from "react-router-dom"
 function Settings() {
     return (
     <>
-    {/*Logo & nav bar*/}
+    {/*Logo & nav bar should go up at the top!!!*/}
+    {/*I feel like i can definitely replace 80 with a const but thats a problem for later...*/}
+    
     <div className="w-dvw h-dvh flex flex-col align-center items-center gap-y-[5%]">
-        {/*---Settings---*/}
+        {/*---Settings Section---*/}
         <h1 className= "pt-[2%] text-center">Settings</h1>
-        <Separator className="w-[60%]"></Separator>
 
         {/*---User Details---*/}
-        <Button>
-            User Details
-        <ChevronRight className="w-vw h-vh" />
-        </Button>
+        <Link to="./user" className={" w-[80%] " + buttonVariants({ variant: "oblong" })}>
+            User details
+            <ChevronRight className="w-vw h-vh" />
+        </Link>
 
         {/*---Form Autofill Details---*/}
-        <Button>Form Autofill Details
-        <ChevronRight className="w-vw h-vh" />
-        </Button>
+        <Link to="/settings" className={" w-[80%] " + buttonVariants({ variant: "oblong" })}>
+            Form autofill details
+            <ChevronRight className="w-vw h-vh" />
+        </Link>
         
-        {/*---Support---*/}
+        {/*---Support Section---*/}
         <h1 className= "text-center">Support</h1>
-        <Separator className="w-[60%]"></Separator>
 
         {/*---Contact Us---*/}
-        <Button>Contact Us
-        <ChevronRight className="w-vw h-vh" />
-        </Button>
+        <Link to="/settings" className={" w-[80%] " + buttonVariants({ variant: "oblong" })}>
+            Contact us
+            <ChevronRight className="w-vw h-vh" />
+        </Link>
 
         {/*---About---*/}
-        <Button>About
-        <ChevronRight className="w-vw h-vh" />
-        </Button>
+        <Link to="/settings" className={" w-[80%] " + buttonVariants({ variant: "oblong" })}>
+            About
+            <ChevronRight className="w-vw h-vh" />
+        </Link>
+
+        {/*---Log Out---*/}
+        <a href="#" className="text-primary hover:text-primary-hover">Log out</a>
     </div>
     </>
     )
