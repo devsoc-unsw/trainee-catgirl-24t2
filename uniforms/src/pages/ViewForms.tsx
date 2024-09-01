963dd1538b9dece3f0e674f31aaf12ffda01d73a
Ricky Guan <rickyguan1@gmail.com>
2024-09-01 12:22:19 +1000

    made navbar and card component

---
+++ uniforms/src/pages/ViewForms.tsx
@@ -0,0 +1,13 @@
+import { NavBar } from "@/components/ui/navbar"
+import { Card } from "@/components/ui/card"
+
+function ViewForms () {
+    return (
+        <div className="flex flex-row h-[100vh]">
+        <NavBar></NavBar>
+        <Card imageUrl=""></Card>
+        </div>
+    )
+}
+
+export default ViewForms
