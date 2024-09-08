7760f40115910859558d5dee6fa54dc342238703
りん <31785428+rin-senpai@users.noreply.github.com>
2024-09-08 12:57:56 +1000

    Search example

--- uniforms/src/pages/Forms.tsx
+++ uniforms/src/pages/Forms.tsx
@@ -1,17 +1,23 @@
-import { Button } from "@/components/ui/button"
-import { Switch } from "@/components/ui/switch"
-import { Input } from "@/components/ui/input"
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import { Input } from "@/components/ui/input";
+import { Search } from "@/components/ui/search";
 
 function Forms() {
-    return (
+  const [uwu, owo] = useState("");
+
+  return (
     <>
-    <p>Hiiii!</p>
-    <Button>aaah</Button>
-    <Button variant="destructive">danger ahead!!!</Button>
-    <Switch></Switch>
-    <Input />
+      <p>{uwu}</p>
+      <Button>aaah</Button>
+      <Button variant="destructive">danger ahead!!!</Button>
+      <Switch></Switch>
+      <Input />
+      <Search setState={owo} />
     </>
-    )
+  );
 }
 
-export default Forms
+export default Forms;
