64d6155bec52f25f37a2da8ee56211df8bd00b7d
りん <31785428+rin-senpai@users.noreply.github.com>
2024-09-11 17:31:52 +1000

    epic trol

--- uniforms/src/App.tsx
+++ uniforms/src/App.tsx
@@ -10,47 +10,21 @@ import Email from "./pages/settings/user-details/account/Email";
 import Username from "./pages/settings/user-details/account/Username";
 import Password from "./pages/settings/user-details/account/Password";
 import Delete from "./pages/settings/user-details/account/Delete";
-<<<<<<< Updated upstream
-import FormAutofill from './pages/settings/form-autofill/FormAutofill';
-import ContactUs from './pages/settings/contact-us/ContactUs';
-import Faq from './pages/settings/contact-us/Faq';
-import About from './pages/settings/about/About';
-import Societies from './pages/Societies';
-import Manage from './pages/Manage';
-=======
 import FormAutofill from "./pages/settings/form-autofill/FormAutofill";
 import ContactUs from "./pages/settings/contact-us/ContactUs";
 import Faq from "./pages/settings/contact-us/Faq";
 import About from "./pages/settings/about/About";
->>>>>>> Stashed changes
+import Societies from "./pages/Societies";
+import Manage from "./pages/Manage";
 
 function App() {
   return (
-<<<<<<< Updated upstream
-     <BrowserRouter>
-	<Routes>
-	  <Route path="forms" element={<Forms />} />
-    <Route path="viewforms" element={<ViewForms />} />
-    <Route path="societies" element={<Societies />} />
-    <Route path="manage" element={<Manage />} />
-    <Route path="settings" element={<Settings />} />
-    <Route path="settings/user" element={<UserDetails />} />
-    <Route path="settings/user/account" element={<AccountSettings />} />
-    <Route path="settings/user/forms" element={<FormsSocieties />} />
-    <Route path="settings/user/account/email" element={<Email />} />
-    <Route path="settings/user/account/username" element={<Username />} />
-    <Route path="settings/user/account/password" element={<Password />} />
-    <Route path="settings/user/account/delete" element={<Delete />} />
-    <Route path="settings/autofill" element={<FormAutofill />} />
-    <Route path="settings/contact" element={<ContactUs />} />
-    <Route path="settings/contact/faq" element={<Faq />} />
-    <Route path="settings/about" element={<About />} />
-	</Routes>
-     </BrowserRouter>
-=======
     <BrowserRouter>
       <Routes>
         <Route path="forms" element={<Forms />} />
+        <Route path="viewforms" element={<ViewForms />} />
+        <Route path="societies" element={<Societies />} />
+        <Route path="manage" element={<Manage />} />
         <Route path="settings" element={<Settings />} />
         <Route path="settings/user" element={<UserDetails />} />
         <Route path="settings/user/account" element={<AccountSettings />} />
@@ -63,11 +37,8 @@ function App() {
         <Route path="settings/contact" element={<ContactUs />} />
         <Route path="settings/contact/faq" element={<Faq />} />
         <Route path="settings/about" element={<About />} />
-
-        <Route path="viewforms" element={<ViewForms />} />
       </Routes>
     </BrowserRouter>
->>>>>>> Stashed changes
   );
 }
 
