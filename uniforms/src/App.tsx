093c708cdbfc1c5020c135a33cbb51b8b3113219
りん <31785428+rin-senpai@users.noreply.github.com>
2024-09-11 15:35:38 +1000

    lmao

--- uniforms/src/App.tsx
+++ uniforms/src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import ReactDOM from "react-dom/client";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Forms from "./pages/Forms";
 import Settings from "./pages/Settings";
@@ -14,15 +10,23 @@ import Email from "./pages/settings/user-details/account/Email";
 import Username from "./pages/settings/user-details/account/Username";
 import Password from "./pages/settings/user-details/account/Password";
 import Delete from "./pages/settings/user-details/account/Delete";
+<<<<<<< Updated upstream
 import FormAutofill from './pages/settings/form-autofill/FormAutofill';
 import ContactUs from './pages/settings/contact-us/ContactUs';
 import Faq from './pages/settings/contact-us/Faq';
 import About from './pages/settings/about/About';
 import Societies from './pages/Societies';
 import Manage from './pages/Manage';
+=======
+import FormAutofill from "./pages/settings/form-autofill/FormAutofill";
+import ContactUs from "./pages/settings/contact-us/ContactUs";
+import Faq from "./pages/settings/contact-us/Faq";
+import About from "./pages/settings/about/About";
+>>>>>>> Stashed changes
 
-function App() { 
+function App() {
   return (
+<<<<<<< Updated upstream
      <BrowserRouter>
 	<Routes>
 	  <Route path="forms" element={<Forms />} />
@@ -43,6 +47,27 @@ function App() {
     <Route path="settings/about" element={<About />} />
 	</Routes>
      </BrowserRouter>
+=======
+    <BrowserRouter>
+      <Routes>
+        <Route path="forms" element={<Forms />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="settings/user" element={<UserDetails />} />
+        <Route path="settings/user/account" element={<AccountSettings />} />
+        <Route path="settings/user/forms" element={<FormsSocieties />} />
+        <Route path="settings/user/account/email" element={<Email />} />
+        <Route path="settings/user/account/username" element={<Username />} />
+        <Route path="settings/user/account/password" element={<Password />} />
+        <Route path="settings/user/account/delete" element={<Delete />} />
+        <Route path="settings/autofill" element={<FormAutofill />} />
+        <Route path="settings/contact" element={<ContactUs />} />
+        <Route path="settings/contact/faq" element={<Faq />} />
+        <Route path="settings/about" element={<About />} />
+
+        <Route path="viewforms" element={<ViewForms />} />
+      </Routes>
+    </BrowserRouter>
+>>>>>>> Stashed changes
   );
 }
 
