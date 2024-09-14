5ca0d93421b1821df40f63a6b1dc6f5d08271afe
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-09-14 17:43:50 +1000

    Add token context manager to frontend

---
+++ uniforms/src/contexts/AppContext.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useState } from 'react';
+
+type AppProviderProps = {
+  children: React.ReactNode;
+};
+
+type AppContextType = {
+  token: string | null;
+  updateToken: (newToken: string | null) => void;
+}
+
+const initialAppContext = {
+  token: null,
+  updateToken: () => {}
+};
+
+export const AppContext = createContext<AppContextType>(initialAppContext);
+
+// usage:
+// const { token } = useContext(AppContext)
+const AppProvider = ({ children }: AppProviderProps) => {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+  const updateToken = (newToken: string | null) => {
+    newToken
+      ? localStorage.setItem('token', newToken)
+      : localStorage.removeItem('token');
+    newToken ? setToken(newToken) : setToken(null);
+  };
+
+  return (
+    <AppContext.Provider value={{ token, updateToken }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppProvider;
