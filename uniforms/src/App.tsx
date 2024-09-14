efa0728edb21126c492532cb5f868ddab8cb4ff9
Ricky Guan <z5591153@ad.unsw.edu.au>
2024-09-14 18:20:30 +1000

    finished ViewForms page and fixed Login screen

--- uniforms/src/App.tsx
+++ uniforms/src/App.tsx
@@ -39,7 +39,7 @@ function App() {
     <Route path="settings/contact" element={<ContactUs />} />
     <Route path="settings/contact/faq" element={<Faq />} />
     <Route path="settings/about" element={<About />} />
-
+    <Route path="societies" element={<Societies />} />
     <Route path="viewforms" element={<ViewForms />} />
 	</Routes>
      </BrowserRouter>
